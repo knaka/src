3f2ce049047692fa10dabdecaac5357d22dab596
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-09-06 21:30:14 +0900

    refactor: modernize cowsay script to use new npm package runner
    - Replace direct volta calls with run_npm_pkg function
    - Fix variable naming inconsistencies (remove name_e823b40, fix version variable)
    - Update node version handling to use new --node-version option
    - Switch from volta.lib.sh to node.lib.sh dependency

--- sh/cowsay.sh
+++ sh/cowsay.sh
@@ -4,26 +4,30 @@
 "${sourced_fcad6c8-false}" && return 0; sourced_fcad6c8=true
 
 # cowsay - npm https://www.npmjs.com/package/cowsay
-name_e823b40=cowsay
-version _14ac6ce=1.6.0
+version_14ac6ce=1.6.0
 
 set_cowsay_version() {
   version_14ac6ce="$1"
 }
 
 # Releases · nodejs/node https://github.com/nodejs/node/releases
-node_version_07c311e=24
+cowsay_node_version_07c311e=
 
 set_cowsay_node_version() {
-  node_version_07c311e="$1"
+  cowsay_node_version_07c311e="$1"
 }
 
 set -- "$PWD" "${0%/*}" "$@"; if test "$2" != "$0"; then cd "$2" 2>/dev/null || :; fi
-. ./volta.lib.sh
+. ./node.lib.sh
 cd "$1"; shift 2
 
 cowsay() {
-  volta run --node="$node_version_07c311e" npx --yes --prefer-offline "$name_e823b40@$version_14ac6ce" "$@"
+  set -- "cowsay@$version_14ac6ce" -- "$@"
+  if test -n "$cowsay_node_version_07c311e"
+  then
+    set -- --node-version="$cowsay_node_version_07c311e" "$@"
+  fi
+  run_npm_pkg "$@"
 }
 
 case "${0##*/}" in
