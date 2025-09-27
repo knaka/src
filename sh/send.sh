e14f407444f1ced6c31c10b5486ee85f193ec1ae
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-09-27 23:09:59 +0900

    refactor: reorganize shell scripts and add memoization utilities
    - Remove symlinks for assert.lib.sh and task-sh.sh
    - Add new send.sh script with SSH command functionality
    - Add begin_memoize/end_memoize functions for improved caching
    - Update shell_path function implementation

---
+++ sh/send.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+# vim: set filetype=sh tabstop=2 shiftwidth=2 expandtab :
+# shellcheck shell=sh
+"${sourced_e822ee3-false}" && return 0; sourced_e822ee3=true
+
+set -- "$PWD" "${0%/*}" "$@"; if test "$2" != "$0"; then cd "$2" 2>/dev/null || :; fi
+cd "$1"; shift 2
+
+send() {
+  local ssh_cmd=""
+  OPTIND=1; while getopts _-: OPT
+  do
+    test "$OPT" = - && OPT="${OPTARG%%=*}" && OPTARG="${OPTARG#"$OPT"=}"
+    case "$OPT" in
+      (ssh-command) ssh_cmd="$OPTARG";;
+      (*) echo "Unexpected option: $OPT" >&2; exit 1;;
+    esac
+  done
+  shift $((OPTIND-1))
+
+  $ssh_cmd ls -l /etc
+}
+
+case "${0##*/}" in
+  (send.sh|send)
+    set -o nounset -o errexit
+    send "$@"
+    ;;
+esac
