09f0da073ce337bdbdcd425eb9e91ee84ef92e06
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2025-10-12 19:13:44 +0900

    refactor(fetch): migrate to external net library
    - Remove local fetch.go and fetch_test.go implementations
    - Replace local Fetch function with github.com/knaka/go-utils/net.Fetch
    - Update main.go to use net.Fetch and net.WithVerbose from external library
    - Update go-utils dependency from v0.1.10 to v0.1.11
    - Add net package import for external fetch functionality

--- go/cmd/fetch/main.go
+++ go/cmd/fetch/main.go
@@ -8,6 +8,7 @@ import (
 	//revive:disable-next-line:dot-imports
 	. "github.com/knaka/go-utils"
 	_ "github.com/knaka/go-utils/initwait"
+	"github.com/knaka/go-utils/net"
 )
 
 const appID = "fetch"
@@ -15,7 +16,7 @@ const appID = "fetch"
 func fetchMain(args []string) (err error) {
 	defer Catch(&err)
 	for _, arg := range args {
-		Must(Fetch(arg, WithVerbose(true)))
+		Must(net.Fetch(arg, net.WithVerbose(true)))
 	}
 	return
 }
