fca8bc655f36db7e3a390f83fdb8b752660d62a0
Kiichiro NAKA <knaka@ayutaya.com>
2026-01-29 21:04:10 +0900

    fix: ps1

--- conf/ps1/bak.ps1
+++ conf/ps1/bak.ps1
@@ -1,153 +1,32 @@
-# To permit running scripts. 
-#   Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
-
-# Source this to reflect the changes.
-#  . $PROFILE
-
-$aliases_to_remove = @(
-  "cat",
-  "cd",
-  "cp",
-  "curl",
-  "diff",
-  "gc",
-  "ll",
-  "ls",
-  "mv",
-  "pwd",
-  "rm",
-  "sc",
-  "type",
-  "__7f179f1__"
-)
-
-foreach ($alias in $aliases_to_remove) {
-  if (Test-Path "Alias:$alias") {
-    if ($PSVersionTable.PSVersion.Major -ge 7) {
-      Remove-Alias -Name "$alias" -Force
-    } else {
-      Remove-Item "Alias:$alias" -Force
-    }
-  }
+function symlink {
+  param (
+    [string]$target
+    , [string]$link_path
+  )
+  # Does not work in 5.1 // windows - Symlinks cannot be created in Powershell 5.1 but can be created by Powershell 7 and Command Prompt - Stack Overflow https://stackoverflow.com/questions/66609154/symlinks-cannot-be-created-in-powershell-5-1-but-can-be-created-by-powershell-7   
+  #New-Item -ItemType SymbolicLink -Path $link_path -Target $target
+  cmd.exe /c mklink "$link_path" "$target"
 }
 
-# ==========================================================================
-#region Misc
+function aws_set_profile {
+  param (
+    [string]$prf
+  )
+  $env:AWS_PROFILE = $prf
+}
 
-#endregion
+New-Alias -Name aws-set-profile -Value aws_set_profile -Force
 
-
-function prepend_path {
+function cd {
   param (
     [string]$path
   )
-
-  # パスを正規化
-  $normalizedPath = Resolve-Path -Path $path -ErrorAction SilentlyContinue
-
-  # パスが存在しない場合は終了
-  if (-not $normalizedPath) {
-    return
+  if (-not $path) {
+    $path = $HOME
   }
-
-  # 正規化したパスが現在の PATH に含まれている場合は終了
-  if ($env:PATH -split ';' | Where-Object { $_.Trim() -ne '' } | ForEach-Object { Resolve-Path -Path $_ -ErrorAction SilentlyContinue } | Where-Object { $_ -eq $normalizedPath }) {
-    return
-  }
-
-  # 含まれていない場合のみ追加。`;;` に挟まれた空文字列はカレントディレクトを表してしまうようなので、削除
-  $env:PATH = "$normalizedPath;$env:PATH" -replace ';{2,}', ';'
+  Set-Location $path
 }
 
-# ワイルドカードに一致するディレクトリを取得し、ループ
-Get-ChildItem -Path "$env:USERPROFILE\*-bin" -Directory | ForEach-Object {
-  # それぞれのディレクトリに対して prepend_path を呼ぶ
-  prepend_path $_.FullName
-  # Write-Host $_.FullName
-}
-
-# prepend_path "C:\msys64\usr\bin"
-
-function set_env {
-  param (
-    [string]$key
-    , [string]$val
-  )
-  [System.Environment]::SetEnvironmentVariable($key, $val, "Process")
-}
-
-New-Alias -Name set-env -Value set_env -Force
-
-function aws_switch_profile (
-  [string]$prf_to_set
-) {
-  $prf = aws-switch-profile.cmd $prf_to_set | Out-String
-  $prf = $prf.Trim()
-  [System.Environment]::SetEnvironmentVariable("AWS_PROFILE", $prf, "Process")
-  Clear-Host
-  Write-Output "$$ENV:AWS_PROFILE is set to $prf."
-}
-
-New-Alias -Name aws-switch-profile -Value aws_switch_profile -Force
-
-New-Alias -Name sc -Value sc.cmd -Force
-New-Alias -Name gc -Value gc.cmd -Force
-New-Alias -Name find -Value $ENV:USERPROFILE\busybox-bin\find.exe -Force
-
-# cmd - How to make PowerShell tab completion work like Bash - Stack Overflow https://stackoverflow.com/questions/8264655/how-to-make-powershell-tab-completion-work-like-bash
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-
-$global:y = "$HOME\doc\$(Get-Date -Format "yyyy")"
-$global:dl = "$HOME\Downloads"
-
-function prompt {
-  $lastSuccess = $?
-  # $lastExitCode = $LASTEXITCODE
-
-  # $global:my_pwd = $PWD.Path
-
-  # Git ワークのトップディレクトリ
-  $w = git rev-parse --show-toplevel 2>$null
-  $global:w = $w
-    
-  # 現在のブランチ名
-  $gbranch = git branch --show-current 2>$null
-
-  $width = $Host.UI.RawUI.WindowSize.Width
-  $line = "$($executionContext.SessionState.Path.CurrentLocation)"
-  $rstr = ${gbranch}
-  $padding_num = $width - $line.Length - $rstr.Length
-  $rmargin = " " * $padding_num
-
-  if ($gbranch.Length -gt 0) {
-    $base = basename $gtop
-    $host.UI.RawUI.WindowTitle = "git: $base"
-  } else {
-    $host.UI.RawUI.WindowTitle = "$(Get-Location)"
-  }
-  # return "PS $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) ";
-  if ($lastSuccess) {
-    $bg = "`e[42m" # green
-  } else {
-    $bg = "`e[41m" # red
-  }
-  $fg = "`e[97m"
-  $reset = "`e[0m"
-  
-  "${bg}${fg}${line}${rmargin}${rstr}${reset}`n> "
-}
-
-# If the shell is PowerShell 7, not PowerShell 5
-if ($PSVersionTable.PSVersion.Major -ge 7) {
-  Set-PSReadLineOption -PredictionSource None
-}
-
-if ($PSVersionTable.PSVersion.Major -ge 7) {
-  $PSStyle.FileInfo.Directory = "`e[34;1m"
-  $PSStyle.FileInfo.Executable = "`e[35;1m"
-}
-
-# light-mode だと読めないので、ぜんぶ黒で
 # powershell - How to turn off syntax highlighting in console? - Stack Overflow https://stackoverflow.com/questions/35246709/how-to-turn-off-syntax-highlighting-in-console
 Set-PSReadLineOption -Colors @{
   # None               = 'Black'
@@ -165,17 +44,9 @@ Set-PSReadLineOption -Colors @{
   Default            = 'Black'
 }
 
-# cd の動きに馴染めないので
-function cd {
-  param (
-    [string]$path
-  )
-  # 引数無ければ $HOME へ
-  if (-not $path) {
-    $path = $HOME
-  }
-  # あればそこへ
-  Set-Location $path
+# If the shell is PowerShell 7, not PowerShell 5
+if ($PSVersionTable.PSVersion.Major -ge 7) {
+  Set-PSReadLineOption -PredictionSource None
 }
 
 # How can I modify PowerShell tab-completion to proritise certain file types over others? - Stack Overflow https://stackoverflow.com/questions/75509668/how-can-i-modify-powershell-tab-completion-to-proritise-certain-file-types-over
