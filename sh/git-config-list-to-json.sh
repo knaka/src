451fb53aeb3050d1a301c37319ddafca16daad9b
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2026-01-30 15:06:10 +0900

    chore

---
+++ sh/git-config-list-to-json.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+# vim: set filetype=sh tabstop=2 shiftwidth=2 expandtab :
+# shellcheck shell=sh
+"${sourced_e301601-false}" && return 0; sourced_e301601=true
+
+set -- "$PWD" "${0%/*}" "$@"; if test "$2" != "$0"; then cd "$2" 2>/dev/null || :; fi
+. ./task.sh
+cd "$1"; shift 2
+
+# `git config --list` format to JSON.
+git_config_list_to_json() {
+  # reduce: The reduce syntax allows you to combine all of the results of an expression by accumulating them into a single answer. The form is reduce EXP as $var (INIT; UPDATE).  —  https://jqlang.org/manual/#reduce
+  # inputs: Outputs all remaining inputs, one by one. — https://jqlang.org/manual/#inputs
+  # setpath: The builtin function setpath sets the PATHS in . to VALUE. — https://jqlang.org/manual/#setpath
+  jq --raw-input --slurp --null-input '
+reduce
+  (
+    inputs
+    | split("\n")[]
+    | select(length > 0)
+  ) as $line
+  (
+    {};
+    ($line | split("=")) as $kv
+    | ($kv[0] | split(".")) as $path
+    | setpath($path; $kv[1])
+  )
+'
+}
+
+case "${0##*/}" in
+  (git-config-list-to-json.sh|git-config-list-to-json)
+    set -o nounset -o errexit
+    git_config_list_to_json "$@"
+    ;;
+esac
---
+++ sh/mod.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+# vim: set filetype=sh tabstop=2 shiftwidth=2 expandtab :
+# shellcheck shell=sh
+"${sourced_0c25e20-false}" && return 0; sourced_0c25e20=true
+
+# List git submodules in the project dir.
+
+set -- "$PWD" "${0%/*}" "$@"; if test "$2" != "$0"; then cd "$2" 2>/dev/null || :; fi
+. ./git-config-list-to-json.sh
+cd "$1"; shift 2
+
+mod() {
+  local prj_dir
+  prj_dir="$(git rev-parse --show-toplevel 2>/dev/null)"
+  local submodules
+  submodules="$(git config --file "$prj_dir"/.gitmodules --list | git_config_list_to_json | jq -r '.submodule | keys[]')"
+  # shellcheck disable=SC2086
+  set -- $submodules
+  # todo: show list to choose one?
+  local arg
+  for arg in "$@"
+  do
+    echo "$prj_dir"/"$arg"
+  done
+}
+
+case "${0##*/}" in
+  (mod.sh|mod)
+    set -o nounset -o errexit
+    mod "$@"
+    ;;
+esac
