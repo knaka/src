c81ea01607c289c2da0c49ff29dc1a8483030a32
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2026-02-10 12:43:16 +0900

    chore

--- json2sh.lib.sh
+++ json2sh.lib.sh
@@ -43,12 +43,13 @@ json2sh() {
   done
   shift $((OPTIND-1))
 
-  set -- -r \
+  # shellcheck disable=SC2016
+  local scr='def to_sh(prefix): to_entries[] | $ARGS.named.delim // "__" as $delim | $ARGS.named.local_decl // "" as $local_decl | ( if .key | type == "number" then  .key | tostring else .key | gsub("[-\\.]"; "_") end ) as $shell_key | if .value | type == "object" or type == "array" then .value | to_sh("\(prefix)\($shell_key)\($delim)") else "\($local_decl)\(prefix)\($shell_key)=\"\(.value)\"" end ; . | $ARGS.named.prefix // "json__" as $prefix | to_sh($prefix)' #EMBED: ./json2sh.jq
+  jq -r "$scr" \
     --arg prefix "$prefix" \
     --arg local_decl "$local_decl" \
     --arg delim "$delim" \
     #nop
-  jq -r 'def to_sh(prefix): to_entries[] | $ARGS.named.delim // "__" as $delim | $ARGS.named.local_decl // "" as $local_decl | ( if .key | type == "number" then  .key | tostring else .key | gsub("[-\\.]"; "_") end ) as $shell_key | if .value | type == "object" or type == "array" then .value | to_sh("\(prefix)\($shell_key)\($delim)") else "\($local_decl)\(prefix)\($shell_key)=\"\(.value)\"" end ; . | $ARGS.named.prefix // "json__" as $prefix | to_sh($prefix)' "$@"
 }
 
 # Convert JSON object to shell variable assignment expressions.
