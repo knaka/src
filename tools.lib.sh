edf33d178b24986c905b10889393c153a0045818
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2026-02-12 10:39:17 +0900

    chore

--- tools.lib.sh
+++ tools.lib.sh
@@ -9,17 +9,20 @@ set -- _LIBDIR . "$@"
 shift 2
 cd "$1" || exit 1; shift
 
+# It is preferred that versions of tools are defined not here but by mise and the versions are defined in each mise.toml.
+
 caddy() { mise exec caddy -- caddy "$@"; } 
 chezmoi() { mise exec chezmoi -- chezmoi "$@"; }
 claude() { mise exec "npm:@anthropic-ai/claude-code" -- claude "$@"; }
 gemini() { mise exec "npm:@google/gemini-cli" -- gemini "$@"; }
-ghq() { mise exec "github:x-motemen/ghq" -- ghq "$@"; }
+ghq() { mise exec ghq -- ghq "$@"; }
 go() { mise exec go -- go "$@"; }
 gofmt() { mise exec go -- gofmt "$@"; }
 jmespath() { mise exec jmespath -- jp "$@"; }
+jq() { is_windows && set -- --binary "$@"; mise exec jq -- jq "$@"; }
 lua() { mise exec lua -- lua "$@"; }
 mdpp() { mise exec "github:knaka/mdpp" -- mdpp "$@"; }
-mlr() { mise exec "github:johnkerl/miller" -- mlr "$@"; }
+mlr() { mise exec miller -- mlr "$@"; }
 node() { mise exec node -- node "$@"; }
 npm() { mise exec node -- npm "$@"; }
 npx() { mise exec node -- npx "$@"; }
@@ -27,13 +30,5 @@ peco() { mise exec "go:github.com/knaka/peco/cmd/peco@latest" -- peco "$@"; }
 perl() { mise exec perl -- perl "$@"; }
 python() { mise exec python -- python "$@"; }
 tblcalc() { mise exec "github:knaka/tblcalc" -- tblcalc "$@"; }
+yj() { mise exec yj -- yj "$@"; } # sclevine/yj: CLI - Convert between YAML, TOML, JSON, and HCL. Preserves map order. https://github.com/sclevine/yj
 yq() { mise exec yq -- yq "$@"; }
-yj() { mise exec yj -- yj "$@"; }
-
-jq() { 
-  if is_windows
-  then
-    set -- --binary "$@"
-  fi
-  mise exec jq -- jq "$@"
-}
