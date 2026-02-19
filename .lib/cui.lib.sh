0e0c983b373b14ef48143662dcd853146a8f891b
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2026-02-19 22:12:25 +0900

    feat: hchoose

--- .lib/cui.lib.sh
+++ .lib/cui.lib.sh
@@ -4,6 +4,8 @@
 
 set -- "$PWD" "$@"; if test "${2:+$2}" = _LIBDIR; then cd "$3" || exit 1; fi
 set -- _LIBDIR . "$@"
+. ./utils.lib.sh
+. ./bindump.lib.sh
 . ./commands.lib.sh
 shift 2
 cd "$1" || exit 1; shift
@@ -40,4 +42,84 @@ choose() {
   done
   test -n "$selected" && set -- --selected="$selected" "$@"
   gum choose "$@"
+}
+
+# Horizontal "choose"
+hchoose() {
+  local selected=""
+  local header="Choose:"
+  OPTIND=1; while getopts _-: OPT
+  do
+    test "$OPT" = - && OPT="${OPTARG%%=*}" && OPTARG="${OPTARG#"$OPT"=}"
+    case "$OPT" in
+      (selected) selected="$OPTARG";;
+      (header) header="$OPTARG";;
+      (*) echo "Unexpected option: $OPT" >&2; exit 1;;
+    esac
+  done
+  shift $((OPTIND-1))
+
+  red=$(printf "\033[31m")
+  normal=$(printf "\033[00m")
+
+  local i=1
+  local j=1
+  local arg
+  for arg in "$@"
+  do
+    if test "$arg" = "$selected"
+    then
+      i="$j"
+      break
+    fi
+    j=$((j + 1))
+  done
+
+  while :
+  do
+    printf "%s" "$header" >&2
+    local j=1
+    for arg in "$@"
+    do
+      if test "$i" -eq "$j"
+      then
+        printf "%s[%s]%s" "$red" "$arg" "$normal" >&2
+      else
+        printf " %s " "$arg" >&2
+      fi
+      j=$((j + 1))
+    done
+    printf "\r" >&2
+    local s
+    s="$(get_key)"
+    if test -z "$s"
+    then
+      shift $((i - 1))
+      printf '%s' "$1"
+      echo >&2
+      return
+    fi
+    local x
+    x="$(printf "%s" "$s" | hex_dump)"
+    if test "$x" = "1b "
+    then
+      s="$s$(get_key)$(get_key)"
+      x="$(printf "%s" "$s" | hex_dump)"
+    fi
+    case "$x" in
+      ("1b 5b 44 "|"02 ") # Left
+        if test "$i" -gt 1
+        then
+          i=$((i - 1))
+        fi
+        ;;
+      ("1b 5b 43 "|"06 ") # Right
+        if test "$i" -lt $#
+        then
+          i=$((i + 1))
+        fi
+        ;;
+      (*) ;;
+    esac
+  done
 }
