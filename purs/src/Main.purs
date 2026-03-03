b731865c87255d54a1ad201a81b01a596a7387c8
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2026-03-04 00:15:41 +0900

    chore

---
+++ purs/src/Main.purs
@@ -0,0 +1,12 @@
+module Main where
+
+import Prelude
+
+import Effect (Effect)
+import Effect.Console (log)
+import Test.Assert (assert)
+
+main :: Effect Unit
+main = do
+  log "Hello, PureScript!"
+  assert (100 == 50 * 2 + 1)
