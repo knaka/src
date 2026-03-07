cc946ef4eb460693758c75db15e6e496b456b35a
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2026-03-07 22:02:11 +0900

    chore

--- sh/urlrec.sh
+++ sh/urlrec.sh
@@ -3,6 +3,9 @@
 # shellcheck shell=sh
 "${sourced_102a099-false}" && return 0; sourced_102a099=true
 
+# Fetches the given URL(s) and prints all linked URLs found on each page
+# that are under the same URL prefix (i.e. sub-paths of the given URL).
+
 set -- "$PWD" "${0%/*}" "$@"; if test -z "${_APPDIR-}"; then _APPDIR=.; if test "$2" != "$0"; then _APPDIR="$2"; fi; cd "$_APPDIR" || exit 1; fi
 set -- _LIBDIR .lib "$@"
 . ./.lib/utils.lib.sh
