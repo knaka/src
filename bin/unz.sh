de0aa9a37b221cc46c020d342a3c936ce25112db
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2026-03-25 22:47:00 +0900

    style(unz): use parenthesized case pattern syntax

--- bin/unz.sh
+++ bin/unz.sh
@@ -27,23 +27,23 @@ case "$file" in
 esac
 
 case "$file" in
-  *.ama ) appmod arc extract "$file" ;;
-  *.a ) ar xv "$file" ;;
-  *.tgz | *.tar.gz ) tar zxvf "$file" ;;
-  *.cgz | *.cpio.gz ) gzip -d -c "$file" | cpio --extract --make-directories --verbose ;;
-  *.cpio ) cpio --extract --make-directories --verbose < "$file" ;;
-  *.tbz | tar.bz2 ) tar -x -v -f "$file" --bzip2 ;;
-  *.zip | *.ZIP | *.jar | *.xpi | *.egg | *.war | *.crx | *.ipa | *.xlsx | *.sb3 | *.sprite3 ) unzip "$file" ;;
-  *.tar.lzma ) tar Yxvf "$file" ;;
-  *.tar.Z ) tar Ztvf "$file" ;;
-  *.tar | *.gem ) tar xvf "$file" ;;
-  *.rpm ) rpm2cpio "$file" | cpio --unconditional --extract --make-directories -v ;;
-  *.lzh | *.Lzh | *.LZH ) lha x "%s" ;;
-  *.msi | *.7z ) 7z x "$file" ;;
-  *.rar ) unrar x "$file" ;;
-  *.txz | *.tar.xz ) tar Jxvf "$file" ;;
-  *.phar ) unzphar "$file" ;;
-  * )
+  (*.ama) appmod arc extract "$file" ;;
+  (*.a) ar xv "$file" ;;
+  (*.tgz | *.tar.gz) tar zxvf "$file" ;;
+  (*.cgz | *.cpio.gz) gzip -d -c "$file" | cpio --extract --make-directories --verbose ;;
+  (*.cpio) cpio --extract --make-directories --verbose < "$file" ;;
+  (*.tbz | tar.bz2) tar -x -v -f "$file" --bzip2 ;;
+  (*.zip | *.ZIP | *.jar | *.xpi | *.egg | *.war | *.crx | *.ipa | *.xlsx | *.sb3 | *.sprite3) unzip "$file" ;;
+  (*.tar.lzma) tar Yxvf "$file" ;;
+  (*.tar.Z) tar Ztvf "$file" ;;
+  (*.tar | *.gem) tar xvf "$file" ;;
+  (*.rpm) rpm2cpio "$file" | cpio --unconditional --extract --make-directories -v ;;
+  (*.lzh | *.Lzh | *.LZH) lha x "%s" ;;
+  (*.msi | *.7z) 7z x "$file" ;;
+  (*.rar) unrar x "$file" ;;
+  (*.txz | *.tar.xz) tar Jxvf "$file" ;;
+  (*.phar) unzphar "$file" ;;
+  (*)
     echo Not supported: "$file"
     exit 1
     ;;
