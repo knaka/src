20a5565e00dbb820f909f981b7c0db9cdad9b0ac
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2026-06-30 17:30:49 +0900

    chore: update Mise version to 2026.6.14 and adjust cache directory structure

--- mise.cmd
+++ mise.cmd
@@ -1,6 +1,6 @@
 @REM Home | mise-en-place https://mise.jdx.dev/
 @REM Releases · jdx/mise https://github.com/jdx/mise/releases
-@set mise_ver=2026.6.11
+@set mise_ver=2026.6.14
 
 @echo off
 setlocal enabledelayedexpansion
@@ -14,18 +14,18 @@ if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
   exit /b 1 
 )
 
-set cache_dir_path=%USERPROFILE%\.cache\task-sh\mise@%mise_ver%
+set cache_dir_path=%USERPROFILE%\.cache\mise
 if not exist !cache_dir_path! (
   mkdir "!cache_dir_path!"
 )
-set cmd_name=mise.exe
+set cmd_name=mise-%mise_ver%.exe
 set cmd_path=!cache_dir_path!\!cmd_name!
 if not exist !cmd_path! (
   echo Downloading Mise for Windows. >&2
   set zip_path=!cache_dir_path!\mise.zip
   curl.exe --fail --location --output "!zip_path!" https://github.com/jdx/mise/releases/download/v!mise_ver!/mise-v!mise_ver!-windows-!mise_arch!.zip || exit /b !ERRORLEVEL!
   unzip.exe "!zip_path!" -d !cache_dir_path!\work || exit /b !ERRORLEVEL!
-  move !cache_dir_path!\work\mise\bin\mise.exe "!cache_dir_path!"
+  move !cache_dir_path!\work\mise\bin\mise.exe !cmd_path!
   del /q "!zip_path!"
   del /q /s !cache_dir_path!\work
 )
