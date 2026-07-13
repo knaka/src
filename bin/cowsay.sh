6483c1bb21b0ee27c4f7146763f535c0af8ce087
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2026-07-13 18:37:16 +0900

    fix: improve error handling and consistency in shell scripts

--- bin/cowsay.sh
+++ bin/cowsay.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 # vim: set filetype=sh tabstop=2 shiftwidth=2 expandtab :
 # shellcheck shell=sh
-_() { eval "\${_SOURCED_$1-false}" || ! eval "_SOURCED_$1"=true; }; _ a779d95 && return 0
+_() { eval "\${_LOADED_$1-false}" || ! eval "_LOADED_$1=true"; }; _ 3601669 && return
 
-test "${_APPDIR+set}" = set || { cd "${0%[/\\]*}" 2>/dev/null || cd . || exit 1; _APPDIR="$PWD"; cd "$OLDPWD" || exit 1; } 
-case "${1:+$1}" in (_LIBDIR) cd "$2" || exit 1;; (*) cd "$_APPDIR" || exit 1;; esac; set -- "$OLDPWD" "$@";
+test "${_APPDIR+set}" = set || { cd "${0%[/\\]*}" 2>/dev/null || cd .; _APPDIR="$PWD"; cd "$OLDPWD" || exit; }
+case "${1:+$1}" in (_LIBDIR) cd "$2" || exit;; (*) cd "$_APPDIR" || exit;; esac; set -- "$OLDPWD" "$@";
 set -- _LIBDIR ./.lib "$@"
 . ./.lib/utils.sh
 . ./.lib/commands.sh
 shift 2
-cd "$1" || exit 1; shift
+cd "$1" || exit; shift
 
 # cowsay - npm https://www.npmjs.com/package/cowsay
 cowsay_version_14ac6ce=1.6.0
