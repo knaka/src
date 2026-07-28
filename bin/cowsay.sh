db699c02543ea32bae01ef07e9c5984b5aba56b6
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2026-07-28 13:27:58 +0900

    chore

--- bin/cowsay.sh
+++ bin/cowsay.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 # vim: set filetype=sh tabstop=2 shiftwidth=2 expandtab :
 # shellcheck shell=sh
+# shpp:header
 _() { eval "\${_LOADED_$1-false}" || ! eval "_LOADED_$1=true"; }; _ BIN_COWSAY_SH && return
 
 # shellcheck disable=SC3028,SC3054
-if test "${BASH_VERSION+set}"; then cd "${BASH_SOURCE[0]%[/\\]*}" || cd .; elif test "${1-}" = SCRIPTDIR; then cd "$2" || exit; else cd "${0%[/\\]*}" || cd .; fi 2>/dev/null; set -- SCRIPTDIR ../.lib "$OLDPWD" "$@"
+if test "${BASH_VERSION+set}"; then cd "${BASH_SOURCE[0]%[/\\]*}" || cd .; elif test "${1-}" = SCRIPTDIR; then cd "$2" || exit; else cd "${0%[/\\]*}" || cd .; fi 2>/dev/null; set -- SCRIPTDIR ../.lib "$OLDPWD" "$@" # shpp:source dir=../.lib
 . ../.lib/utils.sh
 . ../.lib/commands.sh
-cd "$3" || exit; shift 3
+cd "$3" || exit; shift 3 # shpp:/source
 
 # cowsay - npm https://www.npmjs.com/package/cowsay
 cowsay_version_14ac6ce=1.6.0
@@ -20,6 +21,7 @@ cowsay() {
   mise exec "npm:cowsay@$cowsay_version_14ac6ce" -- cowsay "$@"
 }
 
+# shpp:guard
 _() { case "${0##*[/\\]}" in ("$1"|"$1".*) ;; (*) false;; esac; }; if _ cowsay
 then
   set -o nounset -o errexit
