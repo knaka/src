36c193b839c4040d100c67b9d625028619d61ada
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2026-08-03 17:47:08 +0900

    fix

--- .config/mise/conf.d/postinstall-msys2.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-cd %MISE_TOOL_INSTALL_PATH%\msys64
-
-C:\Windows\System32\curl.exe -fsSL -o temp.tar.zst https://mirror.msys2.org/msys/x86_64/diffutils-3.12-1-x86_64.pkg.tar.zst
-C:\Windows\System32\tar.exe --zstd -xf temp.tar.zst
-
-del temp.tar.zst
